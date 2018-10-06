8783828051adb8353b3ff2d4b1d35b13ee6df464
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-10-05 20:21:11 -0400

    projects array & contact input fields

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,6 +3,16 @@ import { Link } from 'react-router-dom';
 import { Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
 
 class Contact extends Component {
+    state = {
+        name: '',
+        email: '',
+        content: ''
+    }
+
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
   render() {
     return (
         <fragment>
@@ -11,6 +21,19 @@ class Contact extends Component {
                     <Col xs={12} sm={8} smOffset={2} >
                         <h3 className="contact-text">Let me know what you need</h3>
                     </Col>
+                </Row>
+                <Row>
+                    <form>
+                        <label>Name:
+                            <input type="text" name="name" value={this.state.name} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Email: <input type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+                        </label>
+                        <label>Content:
+                            <input type="text" name="content" value={this.state.content} onChange={this.handleChange} />
+                        </label>
+                    </form>
                 </Row>
             </Grid>
         </fragment>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,36 @@ import { Link } from 'react-router-dom';
 import { Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
 
 class Projects extends Component {
+    state = {
+        projects : [
+            {"name": "Memory game",
+             "tech": "JS",
+             "description": "Card Matching game that uses vanilla javascript",
+             "img": ""
+            },
+            {"name": "Neighborhood Map",
+             "tech": "JS, React",
+             "description": "Map of my local city with selected venues using google maps api and foursquare api",
+             "img": ""
+            },
+            {"name": "Wordpress Site Buisness",
+             "tech": "Wordpress",
+             "description": "Wordpress site for hair salon built with wordpress",
+             "img": ""
+            },
+            {"name": "Wordpress Site Blog",
+             "tech": "Wordpress",
+             "description": "Blog for anime channel built with wordpress",
+             "img": ""
+            },
+            {"name": "This Site",
+             "tech": "JS, React",
+             "description": "Using react, bootstrap, and good old javascript ",
+             "img": ""
+            },
+        ]
+    }
+
   render() {
     return (
         <fragment>
@@ -10,6 +40,24 @@ class Projects extends Component {
                 <Row>
                     <Col xs={12} sm={8} smOffset={2} >
                         <h1>Hello World these are the projects that i have crafted. My attention to detail and everything</h1>
+                    </Col>
+                </Row>
+
+                <Row>
+                    <Col>
+                        <ul>
+                            { this.state.projects.map( (project) => {
+                                return (
+                                    <li>
+                                        <p>{project.name}</p>
+                                        <p>{project.tech}</p>
+                                        <p>{project.description}</p>
+                                    </li>
+                                )
+                            })
+
+                            }
+                        </ul>
                     </Col>
                 </Row>
             </Grid>
