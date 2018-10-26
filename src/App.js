868f46edb18c6c4da418d916a6bd393bbe2ab658
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-10-26 18:20:24 -0400

    landin page added

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ class App extends Component {
             <Router>
                 <div>
                     <Navbar />
-                    <Route exact path="" component={LandingPage} />
+                    <LandingPage />
+                    
                     <Route path="/about" component={About} />
                     <Route path="/projects" component={Projects} />
                     <Route path="/contact" component={Contact} />
