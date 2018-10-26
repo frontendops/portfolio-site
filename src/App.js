d9cd83de80ab607159067ede25ea266ff15c466b
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-10-26 18:27:12 -0400

    projects page moved to main

--- src/App.js
+++ src/App.js
@@ -19,9 +19,9 @@ class App extends Component {
                 <div>
                     <Navbar />
                     <LandingPage />
+                    <Projects />
 
                     <Route path="/about" component={About} />
-                    <Route path="/projects" component={Projects} />
                     <Route path="/contact" component={Contact} />
                     <Footer />
                 </div>
